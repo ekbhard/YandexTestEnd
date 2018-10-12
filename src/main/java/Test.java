d5f24adc0a37f70c67b1f976006fd4224ecb182b
Simon <ekbhard@gmail.com>
2018-10-12 14:19:50 +0500

    v7

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -4,13 +4,27 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
 
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class Test {
     ChromeDriver driver = new ChromeDriver();
-
+//    @AfterTest
+//    public void in() {
+//        System.setProperty("webdriver.chrome.driver", "C:\\Windows\\System32\\chromedriver.exe");
+//        //driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+//        driver.manage().window().maximize();
+//        driver.get("https://www.yandex.ru/");
+//        WebElement InEaill = driver.findElement(By.xpath("//div//div//a[contains(@class,'button desk-notif-card__login-enter')]/span"));
+//        Actions actions = new Actions(driver);
+//        actions.moveToElement(InEaill).click().build().perform();
+//        driver.findElement(By.xpath("//input[@name='login']")).sendKeys("ajajhartd");
+//        driver.findElement(By.xpath("//input[@name='passwd']")).sendKeys("12345df");
+//        driver.findElement(By.xpath("//button[@type='submit']//span[@class='passport-Button-Content']")).click();
+//    }
 
     @org.testng.annotations.Test(groups = {"T1"})
     public void login() {
@@ -24,9 +38,10 @@ public class Test {
         driver.findElement(By.xpath("//input[@name='login']")).sendKeys("ajajhartd");
         driver.findElement(By.xpath("//input[@name='passwd']")).sendKeys("12345df");
         driver.findElement(By.xpath("//button[@type='submit']//span[@class='passport-Button-Content']")).click();
+
     }
 
-    @org.testng.annotations.Test(dependsOnGroups = "T1", groups = {"T3"})
+    @org.testng.annotations.Test(dependsOnGroups = "T1",groups = {"T3"})
     public void change_name() throws InterruptedException {
         WebDriverWait wait = (new WebDriverWait(driver, 5));
         wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//a[@id='nb-3']//*[@class='svgicon svgicon-mail--Settings']")));
@@ -50,8 +65,9 @@ public class Test {
         }
         driver.findElement(By.xpath("//div//div//div//form//button[contains(@class,'nb-button _nb-action-button b-mail-button_setup-saver')]//span[contains(@class,'b-button-text')]")).click();
     }
+
     //dependsOnGroups = "T1"
-    @org.testng.annotations.Test(dependsOnGroups = "T1", groups = {"T3"})
+    @org.testng.annotations.Test(dependsOnGroups = "T1",groups = {"T3"})
     public void send_massage() {
         //login();
         //driver.findElement(By.xpath("//body//button[@class='button2 button2_size_l button2_theme_normal button2_width_max'")).click();
@@ -63,7 +79,7 @@ public class Test {
 
     }
 
-    @org.testng.annotations.Test(dependsOnGroups = "T1", groups = {"T3"})
+    @org.testng.annotations.Test(dependsOnGroups = "T1",groups = {"T3"})
     public void find_my_massages() throws InterruptedException {
         int i;
         List<WebElement> POSTS = driver.findElements(By.xpath("//div[contains(@class,'mail-MessageSnippet-Wrap js-messages-item')]//span[contains(@title,'@')]"));
@@ -74,13 +90,12 @@ public class Test {
                 Actions clac = new Actions(driver);
                 clac.moveToElement(elem).click().build().perform();
             }
-
-            //driver.findElement(By.xpath("//span[@class='mail-Toolbar-Item-Text js-toolbar-item-title js-toolbar-item-title-delete']")).click();
-            //driver.findElement(By.xpath("//div//div//button[contains(@class,'theme_action js-confirm-mops')]")).click();
         }
+        //driver.findElement(By.xpath("//span[@class='mail-Toolbar-Item-Text js-toolbar-item-title js-toolbar-item-title-delete']")).click();
+        //driver.findElement(By.xpath("//div//div//button[contains(@class,'theme_action js-confirm-mops')]")).click();
     }
 
-    @org.testng.annotations.Test(dependsOnGroups = "T1", groups = {"T3"})
+    @org.testng.annotations.Test(dependsOnGroups = "T1",groups = {"T3"})
     public void change_language() {
         WebDriverWait wait = (new WebDriverWait(driver, 5));
         wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//a[@id='nb-3']//*[@class='svgicon svgicon-mail--Settings']")));
@@ -91,10 +106,11 @@ public class Test {
         set.get(0).click();
         driver.findElement(By.xpath("//span[@class='b-selink__link mail-Settings-Lang']")).click();
         driver.findElement(By.xpath("//img[@class='b-mail-icon b-mail-icon_lang-en']")).click();
+        driver.quit();
     }
 
 @org.testng.annotations.Test
-    public void quot() {
+   public void quot() {
         driver.quit();
-    }
+   }
 }
